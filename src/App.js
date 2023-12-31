6f668decfcc32642b1454e9a3708409c8b40386f
BCAvZ <Bartvanzanen@hotmail.com>
2023-12-31 12:35:25 +0100

    Sound enabled

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Fireworks } from '@fireworks-js/react'
+import year from './2024.jpg'
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className={"container"}>
+     <div className="imageWrapper">
+        <img src={year} alt="2024"/>
+     </div>
+      <Fireworks className={"fireworks"}
+
+          options={{
+                    opacity: 0.5,
+                    explosion: 10,
+                    mouse: {
+                        click: true,
+                        max:2,
+                    },
+                    hue: {
+                        min: 0,
+                        max: 360
+                    },
+                    sound: {
+                        enabled:true
+                    }
+          }}
+      />
+    </section>
   );
 }
 
